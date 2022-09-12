0c4ff747f5c3b4249bfb9e2400c19694d17a1bac
Robert Var <mail.robertvari@gmail.com>
2022-09-12 15:19:24 +0200

    update

--- 06_methods.py
+++ 06_methods.py
@@ -8,3 +8,28 @@ class Character:
         self.intelligence = intelligence
         self.wisdom = wisdom
         self.charisma = charisma
+
+    # class method/function
+    def show_status(self):
+        print("-"*50, f"{self.name} Status", "-"*50,)
+        print(f"Name: {self.name}")
+        print(f"Race: {self.race}")
+        print(f"Strength: {self.strength}")
+        print(f"Constitution: {self.constitution}")
+        print(f"Intelligence: {self.intelligence}")
+        print(f"Wisdom: {self.wisdom}")
+        print(f"Charisma: {self.charisma}")
+        print("-" * 100)
+
+    def attack(self, other):
+        print(f"{self.name} attacks {other.name}!!!!")
+
+
+mordred = Character("Mordred", "Robert", "Human")
+# mordred.show_status()
+
+sauron = Character("Sauron", "Gandalf", "Mage", strength=1000, constitution= 100, intelligence=10000, charisma=1)
+# sauron.show_status()
+
+mordred.attack(sauron)
+sauron.attack(mordred)
