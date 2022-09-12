568d87036705622aa4ef2b0b94936b4962b38afa
Robert Var <mail.robertvari@gmail.com>
2022-09-12 15:08:12 +0200

    update

--- 06_methods.py
+++ 06_methods.py
@@ -8,10 +8,3 @@ class Character:
         self.intelligence = intelligence
         self.wisdom = wisdom
         self.charisma = charisma
-
-
-mordred = Character("Mordred", "Robert", "Human")
-print(mordred.name, mordred.player_name, mordred.race, mordred.wisdom)
-
-sauron = Character("Sauron", "Gandalf", "Mage", strength=1000, constitution= 100, intelligence=10000, charisma=1)
-print(sauron.name, sauron.player_name, sauron.charisma)
